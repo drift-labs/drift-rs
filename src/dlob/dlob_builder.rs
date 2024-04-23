fc793e1ee7fd927258f89c331fc642bdcb4196ba
aoikurokawa <ht.so.ik0102@gmail.com>
2024-04-24 07:33:44 +0900

    fix: allow module_inception

--- src/dlob/dlob_builder.rs
+++ src/dlob/dlob_builder.rs
@@ -1,5 +1,5 @@
 use crate::{
-    dlob::DLOB, event_emitter::EventEmitter, slot_subscriber::SlotSubscriber,
+    dlob::dlob::DLOB, event_emitter::EventEmitter, slot_subscriber::SlotSubscriber,
     usermap::UserMap, SdkResult,
 };
 use std::sync::Arc;
